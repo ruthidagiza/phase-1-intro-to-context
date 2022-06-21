a6c428ed0b0af9ca6eb2778a73a8f5c4d368bf8b
IDAGIZA <ruthidagiza@gmail.com>
2022-06-22 01:10:32 +0300

    commit tree c707db2831dcc5d3139f11ff350a39d82e9c0b3e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+let createEmployeeRecord = function(info){
+    return {
+        firstName: info[0],
+        familyName: info[1],
+        title: info[2],
+        payPerHour: info[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+let createEmployeeRecords = function(employeeInfo) {
+    return employeeInfo.map(function(info){
+        return createEmployeeRecord(info)
+    })
+}
+
+let createTimeInEvent = function (employee, clockin){
+    let [date, hour] = clockin.split(' ')
+    employee.timeInEvents.push( {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return employee
+}
